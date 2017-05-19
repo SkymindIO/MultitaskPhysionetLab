db890444e2d262333273f517450c56dad22af6c9
turambar <turinturumarth@gmail.com>
2017-05-18 21:32:41 -0700

    Resolved bug with moving downloaded data files to src/main/resources/physionet2012

--- get_physionet2012_data.sh
+++ get_physionet2012_data.sh
@@ -2,4 +2,4 @@
 
 wget https://skymindacademy.blob.core.windows.net/physionet2012/physionet2012.tar.gz
 tar xzvf physionet2012.tar.gz
-mv -i physionet2012 src/main/resources/
+mv -i physionet2012 src/main/resources/physionet2012
